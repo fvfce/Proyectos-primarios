4c75d078895e6c65c0b5e8a352dad63f1f8a62d4
Juan <causilzapataj@gmail.com>
2026-09-11 16:11:39 -0500

    Se genero la estructura base del proyecto

---
+++ diario-de-ingles/src/App.jsx
@@ -0,0 +1,14 @@
+import Saludar from './temporal'
+
+
+function App() {
+    return (
+        <>
+            <h1>Hello world</h1>
+            <Saludar />
+        </>
+    )
+}
+
+
+export default App; 
